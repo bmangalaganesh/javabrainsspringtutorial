1b583f514b713bf70c06e87f001f874c7a5a26e4
manglu <manglu@gmail.com>
2016-07-28 14:44:29 +1000

    completed "1.10 Bean Autowiring"

--- SpringDemo/src/main/java/org/koushik/javabrains/Triangle.java
+++ SpringDemo/src/main/java/org/koushik/javabrains/Triangle.java
@@ -3,31 +3,44 @@
  */
 package org.koushik.javabrains;
 
-import java.util.List;
-
 /**
  * @author Manglu
  *
  */
-public class Triangle { // extends AbstractShape {
+public class Triangle {
 
-	private List<Point> points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
 	public void draw() {
-
-		System.out.println("Drawing a traing with point co-ordinates:");
-		for (Point aPoint : points) {
-			System.out.println(":" + aPoint);
-		}
+		System.out.println("Drawing a triangle... with points A:" + getPointA() + "  point B:" + getPointB()
+				+ " point C:" + getPointC());
 
 	}
 
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 }
