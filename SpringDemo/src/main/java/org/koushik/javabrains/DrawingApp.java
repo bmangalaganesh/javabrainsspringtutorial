cef107656808d175d6d41a12abb9f0442f2f3dd6
manglu <manglu@gmail.com>
2016-07-28 14:00:00 +1000

    completed tutorial - "1.7 Injecting Objects"

--- SpringDemo/src/main/java/org/koushik/javabrains/DrawingApp.java
+++ SpringDemo/src/main/java/org/koushik/javabrains/DrawingApp.java
@@ -5,7 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Following the SPring Tutorial on JavaBrains.
- * {@link} https://javabrains.io/courses/spring_core/lessons/Writing-Code-Using-the-Bean-Factory
+ * {@link} https://javabrains.io/courses/spring_core/lessons/Writing-Code-Using-
+ * the-Bean-Factory
+ * 
  * @author Manglu
  *
  */
@@ -13,18 +15,22 @@ public class DrawingApp {
 
 	/**
 	 * 
-	 * Started off with the XMLBeanFactory (which is deprecated) and then replaced that with the AplicationContext which is the replacement
+	 * Started off with the XMLBeanFactory (which is deprecated) and then
+	 * replaced that with the AplicationContext which is the replacement
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		//XmlBeanFactory has been deprecated as I follow this tutorial, i shodul also find the replacement component
-		//Ideally  I would move away from XML and use annotations only..
-		
-		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		
-		ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
-		Shape aShape = (Shape) context.getBean("triangle");
+
+		// XmlBeanFactory has been deprecated as I follow this tutorial, i
+		// shodul also find the replacement component
+		// Ideally I would move away from XML and use annotations only..
+
+		// BeanFactory factory = new XmlBeanFactory(new
+		// FileSystemResource("spring.xml"));
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle aShape = (Triangle) context.getBean("triangle");
 
 		aShape.draw();
 
--- SpringDemo/src/main/java/org/koushik/javabrains/Triangle.java
+++ SpringDemo/src/main/java/org/koushik/javabrains/Triangle.java
@@ -7,21 +7,40 @@ package org.koushik.javabrains;
  * @author Manglu
  *
  */
-public class Triangle extends AbstractShape {
+public class Triangle { // extends AbstractShape {
 
-	public Triangle(String type) {
-		this.type = type;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public void draw() {
+		System.out.println("Drawing a triangle... with points A:" + getPointA() + "  point B:" + getPointB()
+				+ " point C:" + getPointC());
+
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.koushik.javabrains.Shape#draw()
-	 */
-	@Override
-	public void draw() {
-		System.out.println("Drawing a triangle... of type:" + getType());
+	public Point getPointA() {
+		return pointA;
+	}
 
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 }
